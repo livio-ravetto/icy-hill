b02ce9081552abc0bc9a9717a66d3a386133a721
Livio Ravetto <livio.ravetto@external.dekra.com>
2024-03-29 14:37:25 +0000

    Simple change

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Welcome to my app</h1>
+          <MyButton/>
+        </div>
       </header>
     </div>
   );
+
+  function MyButton() {
+    return (
+        <button>
+          I'm a button
+        </button>
+    );
+  }
+
+
 }
 
 export default App;
