355a78888e6bc093a141134519615d41f8ea0f4c
Livio Ravetto <livio.ravetto@external.dekra.com>
2024-03-29 14:38:17 +0000

    Simple change

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <h1>Welcome to my app</h1>
-          <MyButton/>
-        </div>
-      </header>
-    </div>
-  );
-
-  function MyButton() {
     return (
-        <button>
-          I'm a button
-        </button>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div>
+                    <h1>Welcome to my app</h1>
+                    <MyButton/>
+                </div>
+            </header>
+        </div>
     );
-  }
+
+    function MyButton() {
+        return (
+            <button>
+                I'm a button
+            </button>
+        );
+    }
 
 
 }
